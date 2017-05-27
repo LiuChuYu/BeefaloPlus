c7c4b55fb93b1041b074e6fa25125a4d83f8db6b
Ps-Pencil <pan.song@dhs.sg>
2017-05-27 13:09:32 +0100

    remove some unnecessary stuff

--- modmain.lua
+++ modmain.lua
@@ -1,21 +1,5 @@
 local ACTIONS = GLOBAL.ACTIONS
 
-function Set (list)
-  local set = {}
-  for _, l in ipairs(list) do set[l] = true end
-  return set
-end
-
--- List of actions not make sense while rideing
-local exclude = Set {"HAUNT"}
-for k, v in pairs(ACTIONS) do
-  if not exclude[k]  then
-    v.mount_valid = true
-  end
-end
-
-
--- fix pressing space not working
 local function RiderPostInit(self)
   self.SetActionFilter = function(riding)
     end
